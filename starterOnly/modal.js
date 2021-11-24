75d181a60be24659a108e596d242cc5b21b278df
nebulot elodie <nebelodie@gmail.com>
2021-11-24 17:40:17 +0100

    ajout bouton fermer pour le message de fin du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,34 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const clickBtn = document.querySelectorAll(".click-btn");
+const clickEnd = document.querySelector(".click-end");
 
-// launch modal event
+// 'click' = "je m'inscris" 
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// lancement du formulaire voir diplay form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// fermer formulaire  via croix
+document.getElementById("closeForm")
+.addEventListener("click", function (close) {
+  clickEnd.style.display = "none";
+  modalbg.style.display = "none";
+});
 
+// Creation de la fonction message erreur 
+
+
+// chargement des messages erreurs dans un ordre bien précis
+document.getElementById("inscription").addEventListener("click", function(msgError) {
+
+// fermer via fermer
+document
+  .querySelector(".click-end")
+  .addEventListener("click", function (close) {
+    clickEnd.style.display = "none";
+    modalbg.style.display = "none";
+  });
