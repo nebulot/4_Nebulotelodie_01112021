1d8eeab894f26d16df6c20d90e11027f71758ea1
nebulot elodie <nebelodie@gmail.com>
2021-12-02 21:03:05 +0100

    constante modal fin et modal formule ajuster

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,35 +1,31 @@
-// la class responsive est rajouté à la class initiale
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
+//(page du formulaire)
 const modalbg = document.querySelector(".bground");
+
+//(btn je m'inscris) + click
 const modalBtn = document.querySelectorAll(".modal-btn");
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// constante spanClose (représente .close) pour fermer la modale (X)
+const spanCloses = document.querySelectorAll(".close");
+spanCloses.forEach((spanClose) =>
+  spanClose.addEventListener("click", closeModal)
+);
+
+// constante span+click-end pour la modal de fin
+const spanClickEnds = document.querySelectorAll(".click-end");
+spanClickEnds.forEach((spanClickEnd) =>
+  spanClickEnd.addEventListener("click, closeModalEnd")
+);
+
 const formData = document.querySelectorAll(".formData");
 const clickBtn = document.querySelectorAll(".click-btn");
 const clickEnd = document.querySelector(".click-end");
-const myForm = document.getElementById("myForm");
-
-// 'click' = "je m'inscris"
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // lancement du formulaire voir diplay form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-// fermer formulaire  via croix
-document
-  .getElementById("closeForm")
-  .addEventListener("click", function (_close) {
-    modalbg.style.display = "none";
-  });
 
 // fermer via fermer
 document
@@ -53,7 +49,9 @@ function validateForm() {
 
   if (first.length >= 2) {
     document.getElementById("firstError").innerHTML =
-      "Veuillez saisir au minimum 2 caractères dans le champ.";
+      "Veuillez saisir au minimum 2 caractères dans le champ." +
+      colorError +
+      "2px solid";
     estValide = false;
   } else {
     document.getElementById("firstError").innerHTML = "";
@@ -109,3 +107,43 @@ function validateForm() {
   }
 }
 
+//fermer le formulaire de saisie et recuperer les informations
+function closeModal() {
+  modalbg.style.display = "none";
+}
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// afficher et fermer la modal de fin :
+function launchModalEnd() {
+  const modalbgEnd = document.querySelector("#bgroundEnd");
+  modalbgEnd.style.display = "flex";
+}
+
+function closeModalEnd() {
+  const modalbgEnd = document.querySelector("#bgroundEnd");
+  modalbgEnd.style.display = "none";
+}
+
+//                           RESPONSIVE                //
+// la class responsive est rajouté à la class initiale
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+//fait disparaitre les erreurs pré enregistrer
+function supprimeError(errorId) {
+  const elementError = document.querySelector(errorId);
+  elementError.style.visibility = "hidden";
+}
+
+function afficheError(errorId) {
+  const elementError = document.querySelector(errorId);
+  elementError.style.visibility = "visible";
+}
